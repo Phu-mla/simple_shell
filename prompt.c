214ced4603a183b1589e0f3b02763f3454301497
Josephat Ngugi <josephatn10@gmail.com>
2023-05-17 14:53:06 +0000

    Ft: Display a prompt to a user

---
+++ prompt.c
@@ -0,0 +1,29 @@
+#include <main.h>
+
+/**
+ * _prompt - Displays a prompt and waits for a user to type a command.
+ * Return: A Pointer to the prompt string
+ */
+char *_prompt()
+{
+	char *lineptr = NULL;
+	size_t line_len = 0;
+	ssize_t chars_read;
+
+	printf("<^_^> ");
+	fflush(stdout);
+	chars_read = getline(&lineptr, &line_len, stdin);
+
+	if (chars_read == -1)
+	{
+		printf("\n");
+		free(lineptr);
+		return (NULL);
+	}
+
+	/* Replace trailing newline character with a NULL byte */
+	lineptr[strcspn(lineptr, "\n")] = '\0';
+
+	return (lineptr);
+}
+
