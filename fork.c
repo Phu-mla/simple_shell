52b9e696be2650b33a7bedb891f6365ca4ed5b0d
Josephat Ngugi <josephatn10@gmail.com>
2023-05-23 17:47:10 +0000

    modularize the fork Function and add a wait helper Function

--- fork.c
+++ fork.c
@@ -1,48 +1,20 @@
 #include "shell.h"
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
 
 /**
- * child - function for child process
- * @fullpath: full path of executable
- * @tokens: tokenized user input
- * Return: 0 on success, -1 on failure
+ * createChildProcess - forks a process and create a new child process
+ *
+ * Return: The pid of the created process
  */
-int child(char *path, char **tokens)
+
+pid_t createChildProcess(void)
 {
-    pid_t child;
-    int status;
-    char **envp = environ;
+	pid_t pid = fork();
 
-    child = fork();
-    if (child == -1)
-    {
-        perror("Fork Error");
-        exit(EXIT_FAILURE);
-    }
+	if (pid == -1)
+	{
+		perror("fork");
+		exit(EXIT_FAILURE);
+	}
 
-    if (child == 0)
-    {
-        
-        if (execve(path, tokens, environ) == -1)
-        {
-            perror("execve Error");
-            exit(EXIT_FAILURE);
-        }
-    }
-    else
-    {
-        
-        if (wait(&status) == -1)
-        {
-            perror("Wait Error");
-            exit(EXIT_FAILURE);
-        }
-    }
-
-    return 0;
+	return (pid);
 }
-
