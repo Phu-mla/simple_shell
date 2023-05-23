9788be76bc8460ca3adf7dd4167530daadffd654
Phumla Moloi <phumlah.m@gmail.com>
2023-05-23 07:05:17 -0700

    added fork and exec functions

--- exec.c
+++ exec.c
@@ -1,16 +1,30 @@
-#include "main.h"
+#include "shell.h"
+#define MAX_ARGS 50
 
 /**
  * runcmd - Handles and Executes a command 
+ * @cmd: command to be executed
+ *
+ * Return : nothing
  */
 void runcmd(char *cmd)
 {
 	char *args[] = {cmd, NULL};
-	execve(cmd, args, NULL);
+	pid_t pid - createChildProcess();
+
+	args[0] = cmd;
+	args[1] = NULL;
+
+	if (pid == 0)
+	{
+		execve(cmd, args, NULL);
 	
 	/* If the execution reaches this point,
 	 * it means the command couldn't be executed
 	 */
 	/* perror(args[0]); */
 	perror("./shell");
+	}
+	else waitForChild();
+
 }
